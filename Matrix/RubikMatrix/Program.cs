932e76d83f636fdb152a10756fad06154d121da0
Emo <eiv.razgrad@gmail.com>
2018-09-23 20:15:52 +0300

    Add solved projects

---
+++ Matrix/RubikMatrix/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+
+namespace RubikMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read data and make matrix
+            var dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+
+            int[][] matrixRubik = new int[rows][];
+
+            GetMatrix(matrixRubik, cols);
+
+            //Print(matrixRubik);
+            int commands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commands; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                var rowCol = int.Parse(input[0]);
+                var direction = input[1];
+                var moves = int.Parse(input[2]);
+                //
+                if (direction=="down")
+                {
+                    MovesDown(matrixRubik, rowCol, moves % matrixRubik.Length);
+                }
+                else if (direction == "left")
+                {
+                    MovesLeft(matrixRubik, rowCol, moves % matrixRubik[0].Length);
+                }
+                else if (direction == "right")
+                {
+                    MovesRight(matrixRubik, rowCol, moves % matrixRubik[0].Length);
+                }
+                else if (direction == "up")
+                {
+                    MovesUp(matrixRubik, rowCol, moves % matrixRubik.Length);
+
+                }               
+                
+            }
+            var counter = 1;
+            for (int row = 0; row < matrixRubik.Length; row++)
+            {
+                for (int col = 0; col < matrixRubik[row].Length; col++)
+                {
+                    if (matrixRubik[row][col] == counter)
+                    {
+                        Console.WriteLine("No swap required");
+                        counter++;
+                    }
+                    else
+                    {
+                        Rearrange(matrixRubik, row, col, counter);
+                        counter++;
+                    }
+                }
+            }
+        }
+
+        private static void Rearrange(int[][] matrixRubik, int row, int col, int counter)
+        {
+            for (int targertRow = 0; targertRow < matrixRubik.Length; targertRow++)
+            {
+                for (int targetCol = 0; targetCol < matrixRubik[targertRow].Length; targetCol++)
+                {
+                    if (matrixRubik[targertRow][targetCol] == counter)
+                    {
+                        matrixRubik[targertRow][targetCol] = matrixRubik[row][col];
+                        matrixRubik[row][col] = counter;
+                        Console.WriteLine($"Swap ({row}, {col}) with ({targertRow}, {targetCol})");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void MovesUp(int[][] matrixRubik, int col, int moves)
+        {
+            for (int move = 0; move < moves; move++)
+            {
+                var last = matrixRubik[0][col];
+                for (int row = 0; row < matrixRubik.Length-1; row++)
+                {
+                    matrixRubik[row][col] = matrixRubik[row + 1][col];
+                }
+                matrixRubik[matrixRubik.Length - 1][col] = last;
+            }
+
+        }
+
+        private static void MovesRight(int[][] matrixRubik, int row, int moves)
+        {
+            for (int move = 0; move < moves; move++)
+            {
+                var lastElement = matrixRubik[row][matrixRubik[row].Length-1];
+
+                for (int col = matrixRubik.Length - 1; col > 0; col--)
+                {
+                    matrixRubik[row][col] = matrixRubik[row][col - 1];
+                }
+                matrixRubik[row][0] = lastElement;
+            }
+  
+        }
+
+        private static void MovesLeft(int[][] matrixRubik, int row, int moves)
+        {
+            for (int move = 0; move < moves; move++)
+            {
+                var firstElemet = matrixRubik[row][0];
+
+                for (int col = 0; col < matrixRubik[row].Length - 1; col++)
+                {
+                    matrixRubik[row][col] = matrixRubik[row][col + 1];
+                }
+                matrixRubik[row][matrixRubik[row].Length - 1] = firstElemet;
+            }
+       
+        }
+
+        private static void MovesDown(int[][] matrixRubik, int col, int moves)
+        {
+            for (int move = 0; move < moves; move++)
+            {
+                var lastElemet = matrixRubik[matrixRubik.Length - 1][col];
+                for (int row = matrixRubik.Length - 1; row > 0; row--)
+                {
+                    matrixRubik[row][col] = matrixRubik[row - 1][col];
+                }
+                matrixRubik[0][col] = lastElemet;
+            }
+       
+        }
+
+        private static void Print(int[][] matrixRubik)
+        {
+            for (int row = 0; row < matrixRubik.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ", matrixRubik[row]));
+            }
+        }
+
+        private static void GetMatrix(int[][] matrixRubik, int cols)
+        {
+            var counter = 1;
+            for (int row = 0; row < matrixRubik.Length; row++)
+            {
+                matrixRubik[row] = new int[cols];
+                for (int col = 0; col < matrixRubik[row].Length; col++)
+                {
+                    matrixRubik[row][col] = counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Matrix/TargetPractice/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TargetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string snake = Console.ReadLine();
+            int[] targets = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            char[][] matrix = new char[rows][];
+
+            GetMatrix(matrix, cols, snake);  
+            Shoot(matrix, targets);
+            Collapse(matrix);
+            Print(matrix);
+
+        }
+
+        private static void Collapse(char[][] matrix)
+        {
+            Queue<char> elements = new Queue<char>(matrix.Length);
+            for (int col = 0; col < matrix[0].Length; col++)
+            {
+                var counter = 0;
+
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    if (matrix[row][col] != ' ')
+                    {
+                        elements.Enqueue(matrix[row][col]);
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+                for (int row = 0; row < counter; row++)
+                {
+                    matrix[row][col] = ' ';
+                }
+                for (int row = counter; row < matrix.Length; row++)
+                {
+                    matrix[row][col] = elements.Dequeue();
+                }
+            }
+          
+        }
+
+        private static void Shoot(char[][] matrix, int[] targets)
+        {
+            var targetRow = targets[0];
+            var targetCol = targets[1];
+            var radius = targets[2];
+
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    bool isInsidePitagorTheory = Math.Pow((targetRow-row), 2)+Math.Pow((targetCol-col),2)<=Math.Pow(radius,2);
+                    if (isInsidePitagorTheory)
+                    {
+                        matrix[row][col] = ' ';
+                    }
+                }
+            }
+        }
+
+        private static void Print(char[][] matrix)
+        {
+            foreach (var item in matrix)
+            {
+                Console.WriteLine(string.Join("", item ));
+
+            }
+        }
+
+        private static void GetMatrix(char[][] matrix, int cols, string snake)
+        {
+            var counter = 0;
+            bool isLeft = true;
+
+            for (int row = matrix.Length - 1; row >= 0; row--)
+            {
+                matrix[row] = new char[cols];
+                if (isLeft)
+                {                 
+                    for (int col = matrix[row].Length - 1; col >= 0; col--)
+                    {
+                        counter = SetLetter(matrix, snake, counter, row, col);
+                    }
+                    isLeft = false;
+                }
+                else
+                {                  
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        counter = SetLetter(matrix, snake, counter, row, col);
+                    }
+                    isLeft = true;
+                }
+              
+
+            }
+
+        }
+
+        private static int SetLetter(char[][] matrix, string snake, int counter, int row, int col)
+        {
+            if (counter > snake.Length - 1)
+            {
+                counter = 0;
+            }
+            matrix[row][col] = snake[counter++];
+            return counter;
+        }
+    }
+}
